49814ac8d8f3aa3098696ee00a28c58698db179c
Erin Chon <echon@bsd220.org>
2022-10-27 09:25:41 -0500

    Initial Commit

---
+++ tic tac toe/tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeApp.swift
+//  tic tac toe
+//
+//  Created by Erin Chon on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
