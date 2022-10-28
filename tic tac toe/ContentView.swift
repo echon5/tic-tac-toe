1a43cfb03a41e57741b2e8606bf94b2e51afd08a
Erin Chon <echon@bsd220.org>
2022-10-28 09:34:57 -0500

    added x's and o's animation

--- tic tac toe/ContentView.swift
+++ tic tac toe/ContentView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = Array(repeating: "", count: 9)
+    @State private var xTurn = true
     var body: some View {
+        
         VStack {
             Text ("Tic Tac Toe")
                 .font(.title)
@@ -17,28 +20,35 @@ struct ContentView: View {
             
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15) {
                 ForEach(0..<9) { index in
+                    
+                    
                     ZStack{
                         Color.blue
-                        Text("X")
+                        Text(moves[index])
                             .font(.system(size: 90))
                             .fontWeight(.heavy)
                     }
                     .frame(width: 120, height: 120, alignment: .center)
                     .cornerRadius (30)
-                   
-                    .frame(width: 120, height: 120, alignment: .center)
-                        .cornerRadius(30)
-                    
+                    .onTapGesture {
+                        withAnimation(.default) {
+                            if moves[index] == "" {
+                                moves[index] = xTurn ? "X" : "O"
+                                xTurn.toggle()
+                            }
+                            
+                        }
+                    }
+                    .padding()
                 }
             }
-            .padding()
-        }
-    }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
         }
     }
 }
+            struct ContentView_Previews: PreviewProvider {
+                static var previews: some View {
+                    ContentView()
+                }
+            }
+
 
